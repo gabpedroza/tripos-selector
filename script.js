ee9a7d7bed6dfc2822572df038897138620d150c
Gabriel Pedroza <172933533+gabpedroza@users.noreply.github.com>
2026-03-17 09:31:38 +0000

    minor fix

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const numProblems = parseInt(dom.numProblemsInput.value, 10) || 3;
         const allTopics = getAllTopics();
-        const allowedModules = dom.allowedModules.value.replace(/\s/g, '').split(',');
+        const allowedModules = dom.allowedModules.value.replace(/\s+/g, '').split(',');
 
         const now = new Date();
         const dueTopics = allTopics.filter(t => {
